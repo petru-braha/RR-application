9d67517555187898ead9ef93a01a68c0c35d8f6f
petru-braha <petrubraha@gmail.com>
2024-11-25 21:23:27 +0200

    README experiment; included some of the files needed;

---
+++ data/data generator.c
@@ -0,0 +1,12 @@
+
+static const int count_cities = 42;
+static const int count_routes_max = 41 * 42 / 2;
+
+int main()
+{
+    /*
+    0. generate at random the number of routes
+    1. generate 
+    
+    */
+}
