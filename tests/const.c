340cb90588d5eb519258f2cdb254f69c802e7791
p <petrubraha@gmail.com>
2025-01-05 16:09:55 +0200

    xml lib install; brainstorm on data storage;

---
+++ tests/const.c
@@ -0,0 +1,6 @@
+int main()
+{
+    const int a = 5;
+    int b = a;
+    const int c = b;
+}
--- tests/xml_read.c
+++ tests/xml_read.c
@@ -1,6 +1,33 @@
-#include <libxml/xmlmemory.h>
+#include <limits.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <libxml2/libxml/xmlmemory.h>
+#include <libxml2/libxml/parser.h>
+#include <libxml2/libxml/tree.h>
 
+#include "../include/route.h"
+
+const char *path = "../include/data/schedule.xml";
+
+// gcc xml_read.c -I/usr/include/libxml2 -lxml2
 int main()
 {
+    
+    // xmlKeepBlanksDefault();
+    printf("%d\n", sizeof(struct rr_routes));
+    xmlDocPtr document = xmlParseFile(path);
 
+    xmlNodePtr entity = NULL;
+    entity = xmlDocGetRootElement(document)->xmlChildrenNode;
+
+    for (; entity; entity = entity->next)
+    {
+        if (0 == xmlStrcmp(entity->name, (const xmlChar *)"text"))
+            continue;
+        printf("%s\n", entity->name);
+    }
+
+    xmlFreeDoc(document);
+    return EXIT_SUCCESS;
 }
